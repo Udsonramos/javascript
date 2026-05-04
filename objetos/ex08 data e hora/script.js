5a57fdd471748b9a8f1e3dd338d35cf08de37a22
Udsonramos <udsonlage7@gmail.com>
2026-05-04 20:20:16 -0300

    estudo de data e hora

---
+++ objetos/ex08 data e hora/script.js
@@ -0,0 +1,19 @@
+const div_relogio = document.querySelector("#div_relogio")
+
+const relogio =()=>{
+    const data = new Date()
+
+    let hora = data.getHours()
+    hora = hora < 10? `0${hora}`:hora
+
+    let minuto = data.getMinutes()
+    minuto = minuto < 0? `0${minuto}`:minuto
+
+    let segundo = data.getSeconds()
+    segundo = segundo < 0? `0${segundo}`:segundo
+
+    let hora_completa = `${hora}:${minuto}:${segundo}`
+    div_relogio.innerHTML = hora_completa
+}
+
+const intervalo = setInterval(relogio,1000)
