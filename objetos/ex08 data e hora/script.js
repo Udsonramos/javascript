e569b01baabaa3862b25c62161daa5be1245d793
Udsonramos <udsonlage7@gmail.com>
2026-05-04 20:38:34 -0300

    att estudo de data e hora

--- objetos/ex08 data e hora/script.js
+++ objetos/ex08 data e hora/script.js
@@ -1,19 +1,30 @@
-const div_relogio = document.querySelector("#div_relogio")
+/*
+    const div_relogio = document.querySelector("#div_relogio")
+
+    const relogio =()=>{
+        const data = new Date()
+
+        let hora = data.getHours()
+        hora = hora < 10? `0${hora}`:hora
+
+        let minuto = data.getMinutes()
+        minuto = minuto < 0? `0${minuto}`:minuto
+
+        let segundo = data.getSeconds()
+        segundo = segundo < 0? `0${segundo}`:segundo
+
+        let hora_completa = `${hora}:${minuto}:${segundo}`
+        div_relogio.innerHTML = hora_completa
+    }
+
+    const intervalo = setInterval(relogio,1000)
+*/
+
+const div_relogio = document.getElementById("div_relogio")
 
 const relogio =()=>{
     const data = new Date()
-
-    let hora = data.getHours()
-    hora = hora < 10? `0${hora}`:hora
-
-    let minuto = data.getMinutes()
-    minuto = minuto < 0? `0${minuto}`:minuto
-
-    let segundo = data.getSeconds()
-    segundo = segundo < 0? `0${segundo}`:segundo
-
-    let hora_completa = `${hora}:${minuto}:${segundo}`
-    div_relogio.innerHTML = hora_completa
+    div_relogio.innerHTML = data.toLocaleTimeString()
 }
 
-const intervalo = setInterval(relogio,1000)
+const intervalo = setInterval(relogio,100)
